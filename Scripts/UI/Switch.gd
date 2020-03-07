b9d07b04d869099e7faf884616cd44fc1b932fda
Stefan Kupresak <stefan_vg@hotmail.com>
2020-03-07 23:45:04 +0100

    finish gauge widget:

--- Scripts/UI/Switch.gd
+++ Scripts/UI/Switch.gd
@@ -1,9 +1,25 @@
 extends Node
 tool
 
-export(Texture) var onState;
-export(Texture) var offState;
+export(Texture) var on_state;
+export(Texture) var off_state;
+export(Texture) var super_state;
+
+var state = 0
 
 func _ready():
 	for child in get_child(0).get_child(1).get_children():
-		child.texture = offState;
+		child.texture = off_state;
+
+func _on_Buy_pressed():
+	state = state + 1
+	var i = 0
+	for child in get_child(0).get_child(1).get_children():
+		if state >= 5:
+			var new_state = state % 5 + 1
+			if i < new_state:
+				child.texture = super_state
+		else:
+			if i < state:
+				child.texture = on_state
+		i += 1
